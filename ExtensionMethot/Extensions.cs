473147c94186e9a4bc497091459a8b3dd29c2ab5
SEM <SEM@DESKTOP-4GKM6HK>
2022-10-04 11:32:28 +0300

    Add project files.

---
+++ ExtensionMethot/Extensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethot
+{
+    public static class Extensions
+    {
+        public static string IlkHarfbuyuk(this string value)
+        {
+            return value.Substring(0, 1).ToUpper() + value.Substring(1);
+
+        }
+        public static string AyAdi(this DateTime date)
+        {
+            int ay = date.Month;
+            string ayYaz=" ";
+            switch (ay)
+            {
+                case 1:
+                    ayYaz ="Ocak"; break;
+                case 2:
+                    ayYaz ="Şubat"; break;
+                case 3:
+                    ayYaz ="Mart"; break;
+                case 4:
+                    ayYaz ="Nisan"; break;
+                case 5:
+                    ayYaz ="Mayıs"; break;
+                case 6:
+                    ayYaz ="Haziran"; break;
+                case 7:
+                    ayYaz ="Temmuz"; break;
+                case 8:
+                    ayYaz ="Ağustos"; break;
+                case 9:
+                    ayYaz ="Eylül"; break;
+                case 10:
+                    ayYaz= "Ekim"; break;
+                case 11:
+                    ayYaz ="Kasım"; break;
+                case 12:
+                    ayYaz="Aralık"; break;
+                default:
+                    Console.WriteLine("Yanlış değer");
+                    break;
+            }
+            return date.Day + " " + ayYaz + " " + date.Year;
+        }
+    }
+}
---
+++ ExtensionMethot/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtensionMethot;
+
+string isim = "mustafa";
+Console.WriteLine(isim.IlkHarfbuyuk());
+
+DateTime tarih = DateTime.Now;
+
+Console.WriteLine(tarih.AyAdi());
+Console.ReadKey();
